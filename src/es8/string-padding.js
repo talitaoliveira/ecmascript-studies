9e5da0f31e731040373e031f0158f912f8888c53
Talita Oliveira <litaa.oliveira@gmail.com>
2018-11-21 23:59:08 -0300

    es8 - string padding

---
+++ src/es8/string-padding.js
@@ -0,0 +1,17 @@
+const firstLine = '=======================================================';
+const lastLine = firstLine;
+const lenghtLines = firstLine.length;
+
+let welcomeText = '⭐️ Hello String Padding! ⭐️';
+const lengthText = welcomeText.length;
+const leftToComplete = lenghtLines - lengthText;
+const halfComplete = leftToComplete/2;
+
+welcomeText = welcomeText.padStart(lenghtLines-halfComplete, '~');
+welcomeText = welcomeText.padEnd(lenghtLines, '~');
+
+console.log(`
+${firstLine}
+${welcomeText}
+${lastLine}
+`);
