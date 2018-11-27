571372b13bd19c2a0cf56604b4fd75159a7061cf
Talita Oliveira <litaa.oliveira@gmail.com>
2018-11-26 21:48:01 -0300

    es8: string padding - add example padStart() - add example padEnd() - add bonus example

--- src/es8/string-padding.js
+++ src/es8/string-padding.js
@@ -1,12 +1,56 @@
 /**
  * STRING PADDING
+ * - When you need a string to reach a specific length.
+ * - Two ways to add characters: padStart() and padEnd()
+ * - Basic usage:
+ * 
+ * padStart(finalLength, padString)
+ * padEnd(finalLength, padString)
  */
 
+let lang1 = 'Javascript';
+let lang2 = 'PHP';
+let lang3 = 'Python';
+let lang4 = 'Java';
+
+let finalLength = 20;
+
 // padStart()
+let allLanguangesPadStart = `
+${lang1.padStart(finalLength, '-')}
+${lang2.padStart(finalLength, '-')}
+${lang3.padStart(finalLength, '-')}
+${lang4.padStart(finalLength, '-')}`;
+
+console.log(allLanguangesPadStart);
+/**
+----------Javascript
+-----------------PHP
+--------------Python
+----------------Java
+ */
+
+
 
 // padEnd()
+let allLanguangesPadEnd = `
+${lang1.padEnd(finalLength, '-')}
+${lang2.padEnd(finalLength, '-')}
+${lang3.padEnd(finalLength, '-')}
+${lang4.padEnd(finalLength, '-')}`;
 
-// EXAMPLE
+console.log(allLanguangesPadEnd);
+/**
+Javascript----------
+PHP-----------------
+Python--------------
+Java----------------
+ */
+
+
+/** 
+ * ⭐️ BONUS EXAMPLE ⭐️
+ * */
 const firstLine = '=======================================================';
 const lastLine = firstLine;
 const lenghtLines = firstLine.length;
@@ -24,3 +68,11 @@ ${firstLine}
 ${welcomeText}
 ${lastLine}
 `);
+
+/**
+
+=======================================================
+~~~~~~~~~~~~~~⭐️ Hello String Padding! ⭐️~~~~~~~~~~~~~~
+=======================================================
+
+ */
