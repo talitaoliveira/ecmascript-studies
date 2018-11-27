b76b02eb776ed39efdb77dff1b251bd0b996c5f4
Talita Oliveira <litaa.oliveira@gmail.com>
2018-11-26 22:27:27 -0300

    es7: exponentitation operator

---
+++ src/es7/exponentiation-operator.js
@@ -0,0 +1,13 @@
+/**
+ * EXPONENTIATION OPERATOR
+ * - Is the same as Math.pow()
+ * - Don't need to use the library Math  
+ */
+
+// using Math library
+let newNumber1 = Math.pow(4, 2);
+console.log(newNumber1); // 16
+
+// not using Math library
+let newNumber2 = 4 ** 2;
+console.log(newNumber2); // 16
