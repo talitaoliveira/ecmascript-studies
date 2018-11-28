ec110e18685db937aea65d5abd9c6d85ae4d6af1
Talita Oliveira <litaa.oliveira@gmail.com>
2018-11-28 18:56:06 -0300

    es9: spread properties

--- src/es9/spread-properties.js
+++ src/es9/spread-properties.js
@@ -1,4 +1,21 @@
 /**
- * REST/SPREAD PROPERTIES
- * - Clone Objects
- *  */
+ * SPREAD PROPERTIES
+ * Using the SPREAD operator "..." to create new objects with properties
+ * of the object passed after the spread operator
+ * */
+
+
+const eventData = { website: 'http://rec.jsday.com.br/', location: 'Faculdade Imaculada conceição' };
+
+const eventDay = { day: 1, month: 12, year: 2018 }
+
+const jsDay = { ...eventData, ...eventDay }
+
+console.log(jsDay); 
+/* 
+{ day: 1
+ location: "Faculdade Imaculada conceição"
+ month: 12
+ website: "http://rec.jsday.com.br/"
+ year: 2018 }
+*/
